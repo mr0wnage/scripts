8daf2bff1c9d98d32fec0ff03a31e6f0d7b4fe86
gLAd <31668505+mr0wnage@users.noreply.github.com>
2021-10-24 15:19:53 +0300

    Create 1p.sh

---
+++ 1p.sh
@@ -0,0 +1,1 @@
+echo; echo "Dont forget to edit:"; echo; echo "Send_msg_toTelBot.sh - token and chat ID"; echo "solana-telegram-delinq.sh - pubkey"; echo; echo "CRONTAB:"; echo; echo "*/15 * * * * /root/solana-telegram-delinq.sh"; echo "*/15 * * * * /root/solana-telegram-sync.sh"
