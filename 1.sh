b8c80da2b460deb81af39a0ed247aaf000a811c5
gLAd <31668505+mr0wnage@users.noreply.github.com>
2021-10-24 15:23:54 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,4 +4,4 @@ wget https://raw.githubusercontent.com/mr0wnage/scripts/main/solana-telegram-syn
 chmod +x /root/Send_msg_toTelBot.sh
 chmod +x /root/solana-telegram-delinq.sh
 chmod +x /root/solana-telegram-sync.sh
-echo; echo "Dont forget to edit:"; echo; echo "Send_msg_toTelBot.sh - token and chat ID"; echo "solana-telegram-delinq.sh - pubkey"; echo; echo "CRONTAB:"; echo; echo "*/15 * * * * /root/solana-telegram-delinq.sh"; echo "*/15 * * * * /root/solana-telegram-sync.sh"
+clear; echo; echo "Dont forget:"; echo; echo "Edit /root/Send_msg_toTelBot.sh - token and chat ID"; echo "Edit /root/solana-telegram-delinq.sh - pubkey"; echo; echo "Add to CRONTAB (via crontab -e):"; echo; echo "*/15 * * * * /root/solana-telegram-delinq.sh"; echo "*/15 * * * * /root/solana-telegram-sync.sh"; echo
