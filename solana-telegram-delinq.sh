fe5ccd6b43074b5ed2fcebaff14a351315997c02
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-10-12 16:37:09 +0300

    Update solana-telegram-delinq.sh

--- solana-telegram-delinq.sh
+++ solana-telegram-delinq.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+SCRIPT_DIR=/root/
+APP_SOLANA=/root/.local/share/solana/install/active_release/bin/solana
+APP_SOLANA_KEYGEN=/root/.local/share/solana/install/active_release/bin/solana-keygen
+KEYS_PATH=/root/solana/validator-keypair.json
+ID_PUBKEY=`${APP_SOLANA_KEYGEN} pubkey ${KEYS_PATH}`
 
-#solana-keygen pubkey ~/solana/validator-keypair.json
-VALIDATOR_ADDR="_________________________"
-
-IS_DELINQUENT=`timeout 30 /root/.local/share/solana/install/active_release/bin/solana validators --output json | grep '"delinquent": true' -A 4 | grep ${VALIDATOR_ADDR}`
+IS_DELINQUENT=`timeout 30 /root/.local/share/solana/install/active_release/bin/solana validators --output json | grep '"delinquent": true' -A 4 | grep ${ID_PUBKEY}`
 
 if [[ -z ${IS_DELINQUENT} ]]
 then
-    echo "`date` node is NOT delinquent!"
+    echo "`date` node ${ID_PUBKEY} is NOT delinquent!"
 else
     echo "`date` ALARM! Node is DELINQUENT!"
-  "/root/Send_msg_toTelBot.sh" "${HOSTNAME} inform you:" "ALARM! Solana validator ${VALIDATOR_ADDR} is delinquent"  2>&1 > /dev/null
+  "/root/Send_msg_toTelBot.sh" "${HOSTNAME} inform you:" "ALARM! Solana validator ${ID_PUBKEY} is delinquent"  2>&1 > /dev/null
 fi
