bc64b335501cfecfd637b11a7879173c9d412cf4
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-10-12 16:40:11 +0300

    Update solana-telegram-sync.sh

--- solana-telegram-sync.sh
+++ solana-telegram-sync.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
- 
-INSYNC=`timeout 30 /root/.local/share/solana/install/active_release/bin/solana catchup /root/solana/validator-keypair.json http://127.0.0.1:8899 | grep caught`
+SCRIPT_DIR=/root/
+APP_SOLANA=/root/.local/share/solana/install/active_release/bin/solana
+APP_SOLANA_KEYGEN=/root/.local/share/solana/install/active_release/bin/solana-keygen
+KEYS_PATH=/root/solana/validator-keypair.json
+ID_PUBKEY=`${APP_SOLANA_KEYGEN} pubkey ${KEYS_PATH}`
+
+INSYNC=`timeout 30 /root/.local/share/solana/install/active_release/bin/solana catchup $KEYS_PATH http://127.0.0.1:8899 | grep caught`
 
 if [[ -z ${INSYNC} ]]
 then
- echo "`date` ALARM! node is out of sync"
+ echo "`date` ALARM! node $ID_PUBKEY is out of sync"
   "/root/Send_msg_toTelBot.sh" "$HOSTNAME inform you:" "ALARM! Solana node is out of sync!"  2>&1 > /dev/null
 else
- echo "`date` node is synced"
+ echo "`date` node $ID_PUBKEY is synced"
 fi
