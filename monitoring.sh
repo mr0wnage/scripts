001c10d89b37c10726a1cf35f2012fa9dbe93234
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-10-27 19:46:59 +0300

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,9 +1,12 @@
 wget https://raw.githubusercontent.com/mr0wnage/scripts/main/Send_msg_toTelBot.sh -O /root/Send_msg_toTelBot.sh
+wget https://raw.githubusercontent.com/mr0wnage/scripts/main/solana-telegram-balance.sh -O /root/solana-telegram-balance.sh
 wget https://raw.githubusercontent.com/mr0wnage/scripts/main/solana-telegram-delinq.sh -O /root/solana-telegram-delinq.sh
 wget https://raw.githubusercontent.com/mr0wnage/scripts/main/solana-telegram-skip.sh -O /root/solana-telegram-skip.sh
 wget https://raw.githubusercontent.com/mr0wnage/scripts/main/solana-telegram-sync.sh -O /root/solana-telegram-sync.sh
+
 chmod +x /root/Send_msg_toTelBot.sh
+chmod +x /root/solana-telegram-balance.sh
 chmod +x /root/solana-telegram-delinq.sh
 chmod +x /root/solana-telegram-skip.sh
 chmod +x /root/solana-telegram-sync.sh
-clear; echo; echo "Dont forget:"; echo; echo "Edit /root/Send_msg_toTelBot.sh - token and chat ID"; echo; echo "Add to CRONTAB (via crontab -e):"; echo; echo "*/15 * * * * /root/solana-telegram-delinq.sh > /dev/null"; echo "0 */6 * * * /root/solana-telegram-skip.sh > /dev/null"; echo "*/15 * * * * /root/solana-telegram-sync.sh > /dev/null"; echo
+clear; echo; echo "Dont forget:"; echo; echo "Edit /root/Send_msg_toTelBot.sh - token and chat ID"; echo; echo "Add to CRONTAB (via crontab -e):"; echo; echo "*/15 * * * * /root/solana-telegram-delinq.sh > /dev/null"; echo "0 */6 * * * /root/solana-telegram-skip.sh > /dev/null"; echo "*/15 * * * * /root/solana-telegram-sync.sh > /dev/null"; echo "* * 1 * * /root/solana-telegram-balance.sh > /dev/null"; echo
