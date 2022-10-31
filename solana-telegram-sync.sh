f8f4037813a69f82902272863ed97c9e243b07d5
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-10-31 11:18:51 +0300

    Update solana-telegram-sync.sh

--- solana-telegram-sync.sh
+++ solana-telegram-sync.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-SCRIPT_DIR=/root/
-APP_SOLANA=/root/.local/share/solana/install/active_release/bin/solana
-APP_SOLANA_KEYGEN=/root/.local/share/solana/install/active_release/bin/solana-keygen
-KEYS_PATH=/root/solana/validator-keypair.json
-ID_PUBKEY=`${APP_SOLANA_KEYGEN} pubkey ${KEYS_PATH}`
+SCRIPT_DIR="/root/"
+APP_SOLANA="/root/.local/share/solana/install/active_release/bin/solana"
+APP_SOLANA_KEYGEN="/root/.local/share/solana/install/active_release/bin/solana-keygen"
+KEY_VALI="/root/solana/validator-keypair.json"
+KEY_VOTE="/root/solana/vote-account-keypair.json"
 
-INSYNC=`timeout 30 /root/.local/share/solana/install/active_release/bin/solana catchup $KEYS_PATH http://127.0.0.1:8899 | grep caught`
+PUBKEY_VALI=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VALI})
+INSYNC=$(timeout 30 ${APP_SOLANA} catchup ${KEY_VALI} http://127.0.0.1:8899 | grep caught)
 
 if [[ -z ${INSYNC} ]]
 then
- echo "`date` ALARM! node $ID_PUBKEY is out of sync"
-  "/root/Send_msg_toTelBot.sh" "$HOSTNAME inform you:" "ALARM! Solana node is out of sync!"  2>&1 > /dev/null
+ echo "`date` ALARM! NODE ${PUBKEY_VALI} is out of sync"
+  "${SCRIPT_DIR}/Send_msg_toTelBot.sh" "$HOSTNAME inform you:" "ALARM! NODE ${PUBKEY_VALI} is out of sync!" 2>&1 > /dev/null
 else
- echo "`date` node $ID_PUBKEY is synced"
+ echo "`date` NODE ${PUBKEY_VALI} is synced!"
 fi
