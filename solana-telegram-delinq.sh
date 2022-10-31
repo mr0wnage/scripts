199bf503d7f69d0ed686fa6bfea0119f2b6085e8
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-10-31 11:27:59 +0300

    Update solana-telegram-delinq.sh

--- solana-telegram-delinq.sh
+++ solana-telegram-delinq.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-SCRIPT_DIR=/root/
-APP_SOLANA=/root/.local/share/solana/install/active_release/bin/solana
-APP_SOLANA_KEYGEN=/root/.local/share/solana/install/active_release/bin/solana-keygen
-KEYS_PATH=/root/solana/validator-keypair.json
-ID_PUBKEY=`${APP_SOLANA_KEYGEN} pubkey ${KEYS_PATH}`
+SCRIPT_DIR="/root/"
+APP_SOLANA="/root/.local/share/solana/install/active_release/bin/solana"
+APP_SOLANA_KEYGEN="/root/.local/share/solana/install/active_release/bin/solana-keygen"
+KEY_VALI="/root/solana/validator-keypair.json"
+KEY_VOTE="/root/solana/vote-account-keypair.json"
 
-IS_DELINQUENT=`timeout 30 /root/.local/share/solana/install/active_release/bin/solana validators --output json | grep '"delinquent": true' -A 4 | grep ${ID_PUBKEY}`
+PUBKEY_VALI=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VALI})
+PUBKEY_VOTE=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VOTE})
+
+IS_DELINQUENT=$(timeout 30 ${APP_SOLANA} validators --output json | grep '"delinquent": true' -A 4 | grep ${PUBKEY_VALI})
 
 if [[ -z ${IS_DELINQUENT} ]]
 then
-    echo "`date` node ${ID_PUBKEY} is NOT delinquent!"
+	echo "`date` NODE ${PUBKEY_VALI} is NOT delinquent!"
 else
-    echo "`date` ALARM! Node is DELINQUENT!"
-  "/root/Send_msg_toTelBot.sh" "${HOSTNAME} inform you:" "ALARM! Solana validator ${ID_PUBKEY} is delinquent"  2>&1 > /dev/null
+	echo "`date` ALARM! NODE ${PUBKEY_VALI} is DELINQUENT!"
+	"${SCRIPT_DIR}/Send_msg_toTelBot.sh" "${HOSTNAME} inform you:" "ALARM! NODE ${PUBKEY_VALI} is DELINQUENT!" 2>&1 > /dev/null
 fi
