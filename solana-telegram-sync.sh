9093c91f124dad9ed318fed732853753f70a6bc6
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-10-31 21:29:07 +0300

    Update solana-telegram-sync.sh

--- solana-telegram-sync.sh
+++ solana-telegram-sync.sh
@@ -6,12 +6,14 @@ KEY_VALI="/root/solana/validator-keypair.json"
 KEY_VOTE="/root/solana/vote-account-keypair.json"
 
 PUBKEY_VALI=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VALI})
+PUBKEY_VOTE=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VOTE})
+
 INSYNC=$(timeout 30 ${APP_SOLANA} catchup ${KEY_VALI} http://127.0.0.1:8899 | grep caught)
 
 if [[ -z ${INSYNC} ]]
 then
- echo "`date` ALARM! NODE ${PUBKEY_VALI} is out of sync"
-  "${SCRIPT_DIR}/Send_msg_toTelBot.sh" "$HOSTNAME inform you:" "ALARM! NODE ${PUBKEY_VALI} is out of sync!" 2>&1 > /dev/null
+	echo "`date` ALARM! NODE ${PUBKEY_VALI} is out of sync!"
+	"${SCRIPT_DIR}/Send_msg_toTelBot.sh" "$HOSTNAME inform you:" "ALARM! NODE ${PUBKEY_VALI} is out of sync!" 2>&1 > /dev/null
 else
- echo "`date` NODE ${PUBKEY_VALI} is synced!"
+	echo "`date` NODE ${PUBKEY_VALI} is synced!"
 fi
