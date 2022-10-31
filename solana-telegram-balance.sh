2077f17b0fd26470916c0d487580f94c0b87c0d3
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-10-31 11:19:11 +0300

    Update solana-telegram-balance.sh

--- solana-telegram-balance.sh
+++ solana-telegram-balance.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 SCRIPT_DIR="/root/"
 APP_SOLANA="/root/.local/share/solana/install/active_release/bin/solana"
+APP_SOLANA_KEYGEN="/root/.local/share/solana/install/active_release/bin/solana-keygen"
 KEY_VALI="/root/solana/validator-keypair.json"
 KEY_VOTE="/root/solana/vote-account-keypair.json"
+
+PUBKEY_VALI=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VALI})
+PUBKEY_VOTE=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VOTE})
 
 VALI_BALANCE=$(${APP_SOLANA} balance -k ${KEY_VALI} | awk '{print $1}')
 VOTE_BALANCE=$(${APP_SOLANA} balance -k ${KEY_VOTE} | awk '{print $1}')
@@ -11,4 +15,5 @@ ACTIVE_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -w "Active Stake:" | awk 
 ACTIVATING_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -i "activates" -B4 | grep Balance | awk '{print $2}' | paste -sd+ | bc)
 DEACTIVATING_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -i "deactivates" -B4 | grep Balance | awk '{print $2}' | paste -sd+ | bc)
 
+echo "`date` $HOSTNAME Validator balance: $VALI_BALANCE Vote balance: $VOTE_BALANCE STAKEs: Active: $ACTIVE_STAKE Activating: $ACTIVATING_STAKE, Deactivating: $DEACTIVATING_STAKE"
 "${SCRIPT_DIR}/Send_msg_toTelBot.sh" "$HOSTNAME inform you:" "Validator balance: $VALI_BALANCE Vote balance: $VOTE_BALANCE STAKEs: Active: $ACTIVE_STAKE Activating: $ACTIVATING_STAKE, Deactivating: $DEACTIVATING_STAKE" 2>&1 > /dev/null
