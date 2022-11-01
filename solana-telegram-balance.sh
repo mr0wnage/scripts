a1d687e24f8737d20ac236014f4f15e8ff47c116
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-11-01 09:52:11 +0300

    Update solana-telegram-balance.sh

--- solana-telegram-balance.sh
+++ solana-telegram-balance.sh
@@ -8,12 +8,12 @@ KEY_VOTE="/root/solana/vote-account-keypair.json"
 PUBKEY_VALI=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VALI})
 PUBKEY_VOTE=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VOTE})
 
-VALI_BALANCE=$(${APP_SOLANA} balance -k ${KEY_VALI} | awk '{print $1}')
-VOTE_BALANCE=$(${APP_SOLANA} balance -k ${KEY_VOTE} | awk '{print $1}')
+VALI_BALANCE=$(${APP_SOLANA} balance -k ${KEY_VALI} | awk '{printf("%d\n", $1+0.5)}')
+VOTE_BALANCE=$(${APP_SOLANA} balance -k ${KEY_VOTE} | awk '{printf("%d\n", $1+0.5)}')
 
-ACTIVE_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -w "Active Stake:" | awk '{print $3}' | awk '{s+=$1} END {print s}')
-ACTIVATING_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -i "activates" -B4 | grep Balance | awk '{print $2}' | paste -sd+ | bc)
-DEACTIVATING_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -i "deactivates" -B4 | grep Balance | awk '{print $2}' | paste -sd+ | bc)
+ACTIVE_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -w "Active Stake:" | awk '{print $3}' | awk '{s+=$1} END {printf("%d\n", s+0.5)}')
+ACTIVATING_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -i "activates" -B4 | grep Balance | awk '{printf("%d\n", $2+0.5)}' | paste -sd+ | bc)
+DEACTIVATING_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -i "deactivates" -B4 | grep Balance | awk '{printf("%d\n", $2+0.5)}' | paste -sd+ | bc)
 
 echo "`date` $HOSTNAME Validator balance: $VALI_BALANCE Vote balance: $VOTE_BALANCE STAKEs: Active: $ACTIVE_STAKE Activating: $ACTIVATING_STAKE, Deactivating: $DEACTIVATING_STAKE"
-"${SCRIPT_DIR}/Send_msg_toTelBot.sh" "$HOSTNAME inform you:" "Validator balance: $VALI_BALANCE Vote balance: $VOTE_BALANCE STAKEs: Active: $ACTIVE_STAKE Activating: $ACTIVATING_STAKE, Deactivating: $DEACTIVATING_STAKE" 2>&1 > /dev/null
+"${SCRIPT_DIR}/Send_msg_toTelBot.sh" "$HOSTNAME inform you:" "BALANCE: validator - $VALI_BALANCE, vote - $VOTE_BALANCE %0ASTAKE: active - $ACTIVE_STAKE, activating - $ACTIVATING_STAKE, deactivating - $DEACTIVATING_STAKE" 2>&1 > /dev/null
