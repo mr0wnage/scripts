0034248670218a78578590b78ce2aeab8dd89e2d
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-11-02 13:20:44 +0300

    Update solana-telegram-balance.sh

--- solana-telegram-balance.sh
+++ solana-telegram-balance.sh
@@ -8,12 +8,10 @@ KEY_VOTE="/root/solana/vote-account-keypair.json"
 PUBKEY_VALI=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VALI})
 PUBKEY_VOTE=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VOTE})
 
-TG1="${HOSTNAME} sol alert!"
-TG2="Pubkey: <b>${PUBKEY_VALI}</b>"
+TG1="${HOSTNAME} <i>SOL</i> info!"
+TG2="Pubkey: <code>${PUBKEY_VALI}</code>"
 TGBOT="${SCRIPT_DIR}/Send_msg_toTelBot.sh"
 
-#VALI_BALANCE=$(${APP_SOLANA} balance -k ${KEY_VALI} | awk '{printf("%d\n", $1+0.5)}')
-#VOTE_BALANCE=$(${APP_SOLANA} balance -k ${KEY_VOTE} | awk '{printf("%d\n", $1+0.5)}')
 VALI_BALANCE=$(${APP_SOLANA} balance -k ${KEY_VALI} | awk '{printf("%.2f \n", $1)}')
 VOTE_BALANCE=$(${APP_SOLANA} balance -k ${KEY_VOTE} | awk '{printf("%.2f \n", $1)}')
 
@@ -21,5 +19,5 @@ ACTIVE_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -w "Active Stake:" | awk 
 ACTIVATING_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -i " activates" -B4 | grep Balance | awk '{printf("%d\n", $2+0.5)}' | paste -sd+ | bc)
 DEACTIVATING_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -i "deactivates" -B4 | grep Balance | awk '{printf("%d\n", $2+0.5)}' | paste -sd+ | bc)
 
-echo "`date` $PUBKEY_VALI BALANCE: validator - $VALI_BALANCE vote - $VOTE_BALANCE STAKE: active - $ACTIVE_STAKE activating - $ACTIVATING_STAKE deactivating - $DEACTIVATING_STAKE"
-"${TGBOT}" "${TG1}" "${TG2}" "BALANCE: validator - $VALI_BALANCE vote - $VOTE_BALANCE %0ASTAKE: active - $ACTIVE_STAKE activating - $ACTIVATING_STAKE deactivating - $DEACTIVATING_STAKE" 2>&1 > /dev/null
+echo "`date` Node $PUBKEY_VALI BALANCE: validator - $VALI_BALANCE vote - $VOTE_BALANCE STAKE: active - $ACTIVE_STAKE activating - $ACTIVATING_STAKE deactivating - $DEACTIVATING_STAKE"
+"${TGBOT}" "${TG1}" "${TG2}" "<b>BALANCE:</b> validator - <code>$VALI_BALANCE</code> vote - <code>$VOTE_BALANCE</code> %0A<b>STAKE:</b> - <code>$ACTIVE_STAKE</code> (activating - <code>$ACTIVATING_STAKE</code> deactivating - <code>$DEACTIVATING_STAKE</code>)" 2>&1 > /dev/null
