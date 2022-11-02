fb949bb64d01defd6615dc63fd57edb5d01bb605
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-11-02 09:42:08 +0300

    Update solana-telegram-delinq.sh

--- solana-telegram-delinq.sh
+++ solana-telegram-delinq.sh
@@ -8,12 +8,17 @@ KEY_VOTE="/root/solana/vote-account-keypair.json"
 PUBKEY_VALI=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VALI})
 PUBKEY_VOTE=$(${APP_SOLANA_KEYGEN} pubkey ${KEY_VOTE})
 
+TG1="${HOSTNAME} delinq alert!"
+TG2="Pubkey: <b>${PUBKEY_VALI}</b>"
+TGBOT="${SCRIPT_DIR}/Send_msg_toTelBot.sh"
+
 IS_DELINQUENT=$(timeout 30 ${APP_SOLANA} validators --output json | grep '"delinquent": true' -A 4 | grep ${PUBKEY_VALI})
 
 if [[ -z ${IS_DELINQUENT} ]]
 then
-	echo "`date` NODE ${PUBKEY_VALI} is NOT delinquent!"
+        echo "`date` Node ${PUBKEY_VALI} is NOT delinquent."
+#        "${TGBOT}" "${TG1}" "${TG2}" "Node is <b>NOT</b> delinquent!" 2>&1 > /dev/null
 else
-	echo "`date` ALARM! NODE ${PUBKEY_VALI} is DELINQUENT!"
-	"${SCRIPT_DIR}/Send_msg_toTelBot.sh" "${HOSTNAME} inform you:" "ALARM! NODE ${PUBKEY_VALI} is DELINQUENT!" 2>&1 > /dev/null
+        echo "`date` ALARM! NODE ${PUBKEY_VALI} is DELINQUENT!!!"
+        "${TGBOT}" "${TG1}" "${TG2$}" "ALARM! Node is <b>DELINQUENT!</b>" 2>&1 > /dev/null
 fi
