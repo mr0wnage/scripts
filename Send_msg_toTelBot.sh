578fd5e89ebb80782c1951969f49fc55dbd0dcc4
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-11-02 09:45:31 +0300

    Update Send_msg_toTelBot.sh

--- Send_msg_toTelBot.sh
+++ Send_msg_toTelBot.sh
@@ -4,10 +4,11 @@ telegram_bot_token="_______REPLACE_ME________"
 telegram_chat_id="_______REPLACE_ME________"
 
 Title="$1"
-Message="$2"
+Message1="$2"
+Message2="$3"
 
 curl -s \
  --data parse_mode=HTML \
  --data chat_id=${telegram_chat_id} \
- --data text="<b>${Title}</b>%0A${Message}" \
+ --data text="<b>${Title}</b>%0A${Message1}%0A${Message2}" \
  --request POST https://api.telegram.org/bot${telegram_bot_token}/sendMessage
