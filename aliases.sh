a8dce7e21f034f5b9def054f76222180a82194c8
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-11-04 14:47:19 +0300

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -3,7 +3,7 @@ alias .skip='echo "DEV API:   $(solana block-production | grep $(solana address)
 alias .validators='solana validators | grep "Stake By Version:" -A 20 -B 8'
 alias .slotsum='solana leader-schedule | grep $(solana address) | wc -l'
 alias .epoch='solana epoch-info'
-alias .journal='journalctl -u solana.service -f --no-hostname | ccze'
+alias .journal='journalctl -u solana.service -f -o cat | ccze'
 alias .snap-mn='systemctl stop solana && docker run -it --rm -v /root/solana/validator-ledger:/root/solana/validator-ledger --user $(id -u):$(id -g) c29r3/solana-snapshot-finder:latest --snapshot_path /root/solana/validator-ledger --max_snapshot_age 1000 --min_download_speed 50 && systemctl restart solana'
 alias .snap-tds='systemctl stop solana && docker run -it --rm -v /root/solana/validator-ledger:/root/solana/validator-ledger --user $(id -u):$(id -g) c29r3/solana-snapshot-finder:latest --snapshot_path /root/solana/validator-ledger --max_snapshot_age 500 --min_download_speed 50 -r http://api.testnet.solana.com && systemctl restart solana'
 
