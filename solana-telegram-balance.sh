491a68820f09268463dde6e47049db81b1d1c1ba
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-11-05 21:32:16 +0300

    Update solana-telegram-balance.sh

--- solana-telegram-balance.sh
+++ solana-telegram-balance.sh
@@ -16,8 +16,8 @@ VALI_BALANCE=$(${APP_SOLANA} balance -k ${KEY_VALI} | awk '{printf("%.2f \n", $1
 VOTE_BALANCE=$(${APP_SOLANA} balance -k ${KEY_VOTE} | awk '{printf("%.2f \n", $1)}')
 
 ACTIVE_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -w "Active Stake:" | awk '{print $3}' | awk '{s+=$1} END {printf("%d\n", s+0.5)}')
-ACTIVATING_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -i " activates" -B4 | grep Balance | awk '{printf("%d\n", $2+0.5)}' | paste -sd+ | bc)
-DEACTIVATING_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -i "deactivates" -B4 | grep Balance | awk '{printf("%d\n", $2+0.5)}' | paste -sd+ | bc)
+ACTIVATING_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -i " activates " -B5 | grep Balance | awk '{printf("%d\n", $2+0.5)}' | paste -sd+ | bc)
+DEACTIVATING_STAKE=$(${APP_SOLANA} stakes ${KEY_VOTE} | grep -i "deactivates " -B4 | grep Balance | awk '{printf("%d\n", $2+0.5)}' | paste -sd+ | bc)
 
 echo "`date` Node $PUBKEY_VALI BALANCE: validator - $VALI_BALANCE vote - $VOTE_BALANCE STAKE: active - $ACTIVE_STAKE (activating - $ACTIVATING_STAKE deactivating - $DEACTIVATING_STAKE)"
 "${TGBOT}" "${TG1}" "${TG2}" "<b>BALANCE:</b> validator - <code>$VALI_BALANCE</code> vote - <code>$VOTE_BALANCE</code> %0A<b>STAKE:</b> - <code>$ACTIVE_STAKE</code> (activating - <code>$ACTIVATING_STAKE</code> deactivating - <code>$DEACTIVATING_STAKE</code>)" 2>&1 > /dev/null
