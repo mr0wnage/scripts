9083879277f69a27cc2dda550f2c2957df32b166
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-11-24 09:33:18 +0300

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -2,8 +2,7 @@ alias .catchup='while true; do time solana catchup /root/solana/validator-keypai
 alias .skip='echo "DEV API:   $(solana block-production | grep $(solana address))"; echo "Local API: $(solana block-production --url http://localhost:8899 | grep $(solana address))"'
 alias .validators='solana validators | grep "Stake By Version:" -A 20 -B 8'
 alias .slotsum='solana leader-schedule | grep $(solana address) | wc -l'
-alias .slotcurtds='while true; do solana slot -ut; sleep 5;done'
-alias .slotcurmn='while true; do solana slot -um; sleep 5;done'
+alias .slot='while true; do solana slot; sleep 5; done'
 alias .epoch='solana epoch-info'
 alias .journal='journalctl -u solana.service -f -o cat | ccze'
 alias .snap-mn='systemctl stop solana && docker run -it --rm -v /root/solana/validator-ledger:/root/solana/validator-ledger --user $(id -u):$(id -g) c29r3/solana-snapshot-finder:latest --snapshot_path /root/solana/validator-ledger --max_snapshot_age 1000 --min_download_speed 50 && systemctl restart solana'
@@ -11,6 +10,10 @@ alias .snap-tds='systemctl stop solana && docker run -it --rm -v /root/solana/va
 
 alias .mhz='watch -n 1 "cat /proc/cpuinfo | grep -i mhz"'
 
+alias .balance='solana balance -k /root/solana/validator-keypair.json && solana balance -k /root/solana/vote-account-keypair.json && solana balance ??????????????????'
 
+alias .stakes="echo 'Active Stake:'; .stakes_active; echo 'Activating Stake:'; .stakes_activates; echo 'Deactivating Stake:'; .stakes_deactivates"
 
-alias .balance='solana balance -k /root/solana/validator-keypair.json && solana balance -k /root/solana/vote-account-keypair.json && solana balance ????????????'
+alias .stakes_active="solana stakes /root/solana/vote-account-keypair.json | grep -w 'Active Stake:' | awk '{print \$3}' | awk '{s+=\$1} END {print s}'"
+alias .stakes_activates="solana stakes /root/solana/vote-account-keypair.json | grep -i ' activates ' -A5 -B5 | grep Activating | awk '{s+=\$3} END {print s}'"
+alias .stakes_deactivates="solana stakes /root/solana/vote-account-keypair.json | grep -i 'deactivates' -A5 -B5 | grep Balance | awk '{s+=\$2} END {print s}'"
