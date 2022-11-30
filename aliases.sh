46782037bd054e739e4dcf0796e1a8a58d5bf9f4
gLAd <31668505+mr0wnage@users.noreply.github.com>
2022-11-30 12:34:59 +0300

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -7,6 +7,7 @@ alias .epoch='solana epoch-info'
 alias .journal='journalctl -u solana.service -f -o cat | ccze'
 alias .snap-mn='systemctl stop solana && docker run -it --rm -v /root/solana/validator-ledger:/root/solana/validator-ledger --user $(id -u):$(id -g) c29r3/solana-snapshot-finder:latest --snapshot_path /root/solana/validator-ledger --max_snapshot_age 1000 --min_download_speed 50 && systemctl restart solana'
 alias .snap-tds='systemctl stop solana && docker run -it --rm -v /root/solana/validator-ledger:/root/solana/validator-ledger --user $(id -u):$(id -g) c29r3/solana-snapshot-finder:latest --snapshot_path /root/solana/validator-ledger --max_snapshot_age 500 --min_download_speed 50 -r http://api.testnet.solana.com && systemctl restart solana'
+alias .snap-update='docker pull c29r3/solana-snapshot-finder:latest'
 
 alias .mhz='watch -n 1 "cat /proc/cpuinfo | grep -i mhz"'
 
