419ea224f8e034316334eace8943c43b0f06f159
gLAd <31668505+mr0wnage@users.noreply.github.com>
2024-06-12 18:15:21 +0300

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -7,13 +7,17 @@ alias .epoch='solana epoch-info'
 alias .monitor='solana-validator --ledger /root/solana/validator-ledger/ monitor'
 alias .credits='solana validators -ul --sort=credits -r -n | grep $(solana address)'
 alias .creditsfull='solana validators -ul --sort=credits -r -n | sed -n 1,101p && solana validators -ul --sort=credits -r -n | grep -e $(solana address)'
-
 alias .journal='journalctl -u solana.service -f -o cat | ccze'
-alias .snap-mn='systemctl stop solana && docker run -it --rm -v /root/solana/validator-ledger:/root/solana/validator-ledger --user $(id -u):$(id -g) c29r3/solana-snapshot-finder:latest --snapshot_path /root/solana/validator-ledger --max_snapshot_age 500 --min_download_speed 70 && systemctl restart solana'
-alias .snap-tds='systemctl stop solana && docker run -it --rm -v /root/solana/validator-ledger:/root/solana/validator-ledger --user $(id -u):$(id -g) c29r3/solana-snapshot-finder:latest --snapshot_path /root/solana/validator-ledger --max_snapshot_age 500 --min_download_speed 70 -r http://api.testnet.solana.com && systemctl restart solana'
+alias .snap-mn='systemctl stop solana && docker run -it --rm -v /root/solana/validator-ledger:/root/solana/validator-ledger --user $(id -u):$(id -g) c29r3/solana-snapshot-finder:latest --snapshot_path /root/solana/validator-ledger --max_snapshot_age 500 --min_download_speed 70 && systemctl start solana.service'
+alias .snap-tds='systemctl stop solana && docker run -it --rm -v /root/solana/validator-ledger:/root/solana/validator-ledger --user $(id -u):$(id -g) c29r3/solana-snapshot-finder:latest --snapshot_path /root/solana/validator-ledger --max_snapshot_age 500 --min_download_speed 50 -r http://api.testnet.solana.com && systemctl start solana.service'
 alias .snap-update='docker pull c29r3/solana-snapshot-finder:latest'
 
 alias .mhz='watch -n 1 "cat /proc/cpuinfo | grep -i mhz"'
+
+alias .stakes="echo 'Active Stake:'; .stakes_active; echo 'Activating Stake:'; .stakes_activates; echo 'Deactivating Stake:'; .stakes_deactivates"
+alias .stakes_active="solana stakes /root/solana/vote-account-keypair.json | grep -w 'Active Stake:' | awk '{print \$3}' | awk '{s+=\$1} END {print s}'"
+alias .stakes_activates="solana stakes /root/solana/vote-account-keypair.json | grep -i ' activates ' -A5 -B5 | grep Activating | awk '{s+=\$3} END {print s}'"
+alias .stakes_deactivates="solana stakes /root/solana/vote-account-keypair.json | grep -i 'deactivates' -A5 -B5 | grep Balance | awk '{s+=\$2} END {print s}'"
 
 alias .balance='.balance_i && .balance_v && .balance_r && .balance_j'
 alias .balance_i='echo "identity:  $(solana balance -k /root/solana/validator-keypair.json)"'
