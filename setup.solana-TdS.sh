0f14831cdaaa1a70a6e73b5a5522300111d32c04
gLAd <31668505+mr0wnage@users.noreply.github.com>
2024-08-20 13:17:52 +0300

    Update setup.solana-TdS.sh

--- setup.solana-TdS.sh
+++ setup.solana-TdS.sh
@@ -17,12 +17,30 @@ vm.max_map_count = 1000000
 # Increase number of allowed open file descriptors
 fs.nr_open = 1000000
 EOF"
+
+bash -c "cat >/etc/sysctl.d/21-agave-validator.conf <<EOF
+# Increase UDP buffer sizes
+net.core.rmem_default = 134217728
+net.core.rmem_max = 134217728
+net.core.wmem_default = 134217728
+net.core.wmem_max = 134217728
+
+# Increase memory mapped files limit
+vm.max_map_count = 1000000
+
+# Increase number of allowed open file descriptors
+fs.nr_open = 1000000
+EOF"
 #
 sysctl -p /etc/sysctl.d/21-solana-validator.conf
+sysctl -p /etc/sysctl.d/21-agave-validator.conf
+
 ###
 nano /etc/systemd/system.conf
 # Add
+[Service]
 LimitNOFILE=1000000
+[Manager]
 DefaultLimitNOFILE=1000000
 #
 systemctl daemon-reload
